c1fc0ba582e24dedef2cbfba8e8a3aca98ea40de
Brian Martin <bm@palaucir.cloud>
2026-01-03 14:56:10 -0500

    My Changes.

--- src/main/java/dmn/coding/bmartin/model/User.java
+++ src/main/java/dmn/coding/bmartin/model/User.java
@@ -1,19 +1,18 @@
-package guru.springframework.model;
+package dmn.coding.bmartin.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Created by jt on 2018-12-08.
- */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class User {
+
     private String firstName;
     private String lastName;
     private String email;
+
 }
